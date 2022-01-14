123901197fcaa29a647f2ad2b6b11c3c3f7c9c43
hecuhecu <hecunyuji5@gmail.com>
2022-01-14 11:48:33 +0900

    Initial Commit

---
+++ hirokiKadai7/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hirokiKadai7
+//
+//  Created by 河村宇記 on 2022/01/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hirokiKadai7/hirokiKadai7App.swift
@@ -0,0 +1,17 @@
+//
+//  hirokiKadai7App.swift
+//  hirokiKadai7
+//
+//  Created by 河村宇記 on 2022/01/14.
+//
+
+import SwiftUI
+
+@main
+struct hirokiKadai7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

197d033e70d27b9cae940777e5bd722f47ccb0d5
akio0911 <akio0911@gmail.com>
2022-01-16 11:51:11 +0900

    feedback

--- hirokiKadai7/ContentView.swift
+++ hirokiKadai7/ContentView.swift
@@ -1,51 +1,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var num1: Int?
-    @State private var num2: Int?
-    @State private var num3: Int?
-    @State private var num4: Int?
-    @State private var sum: Int = 0
-    @State private var difference: Int = 0
-    @FocusState private var focus: Bool
-    
     var body: some View {
         TabView {
-            ZStack {
-                Color.orange.edgesIgnoringSafeArea(.top)
-                VStack {
-                    NumberInputView(num: $num1).focused($focus)
-                    NumberInputView(num: $num2).focused($focus)
-                    Button(action: {
-                        sum = (num1 ?? 0) + (num2 ?? 0)
-                        focus = false
-                    }) {
-                        Text("Button")
-                    }
-                    .padding()
-                    
-                    Text("\(sum)")
+            CalculationView(color: .orange, calculate: +)
+                .tabItem{ Text("item1") }
+
+            CalculationView(color: .mint, calculate: -)
+                .tabItem{ Text("item2") }
+        }
+    }
+}
+
+struct CalculationView: View {
+    @State private var num1: Int?
+    @State private var num2: Int?
+    @State private var result: Int = 0
+    @FocusState private var focus: Bool
+
+    let color: Color
+    let calculate: (Int, Int) -> Int
+
+    var body: some View {
+        ZStack {
+            color.edgesIgnoringSafeArea(.top)
+            VStack {
+                NumberInputView(num: $num1).focused($focus)
+                NumberInputView(num: $num2).focused($focus)
+                Button(action: {
+                    result = calculate(num1 ?? 0, num2 ?? 0)
+                    focus = false
+                }) {
+                    Text("Button")
                 }
+                .padding()
+
+                Text("\(result)")
             }
-            .tabItem{ Text("item1") }
-            
-            ZStack {
-                Color.mint.edgesIgnoringSafeArea(.top)
-                VStack {
-                    NumberInputView(num: $num3).focused($focus)
-                    NumberInputView(num: $num4).focused($focus)
-                    Button(action: {
-                        difference = (num3 ?? 0) - (num4 ?? 0)
-                        focus = false
-                    }) {
-                        Text("Button")
-                    }
-                    .padding()
-                    
-                    Text("\(difference)")
-                }
-            }
-            .tabItem{ Text("item2") }
         }
     }
 }
